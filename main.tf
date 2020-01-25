1d0172306ce8ebac1ed3fbcdf075f73b5f51970c
Souissi Mohamed <msouissi@amfinesoft.com>
2020-01-25 23:30:28 +0100

    base de donnée avec terraform

--- main.tf
+++ main.tf
@@ -31,8 +31,26 @@ resource "azurerm_app_service" "project_cloud" {
     location            = "${azurerm_resource_group.project_cloud.location}"
     resource_group_name = "${azurerm_resource_group.project_cloud.name}"
     app_service_plan_id = "${azurerm_app_service_plan.project_cloud.id}"
-    app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "10 LTS"
+}
+
+
+resource "azurerm_sql_server" "project_cloud" {
+  name                         = "mysqlservercloudx"
+  resource_group_name          = "${azurerm_resource_group.project_cloud.name}"
+  location                     = "${azurerm_resource_group.project_cloud.location}"
+  version                      = "12.0"
+  administrator_login          = "ad007min"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_sql_database" "project_cloud" {
+  name                = "mysqldatabase"
+  resource_group_name = "${azurerm_resource_group.project_cloud.name}"
+  location            = "${azurerm_resource_group.project_cloud.location}"
+  server_name         = "${azurerm_sql_server.project_cloud.name}"
+
+  tags = {
+    environment = "production"
   }
 }
 
---
+++ public/files/1_cloud.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Sam 25 Janvier 2020 à 20:45
+-- Version du serveur :  5.7.28-0ubuntu0.18.04.4
+-- Version de PHP :  7.2.24-0ubuntu0.18.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `cloud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL,
+  `fileName` varchar(500) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `file`
+--
+
+INSERT INTO `file` (`id`, `fileName`, `userId`) VALUES
+(8, '22815132_880086155480716_8742390530714896771_n.jpg', 1),
+(9, 'alias.png', 1),
+(10, 'offset.png', 1),
+(11, 'instru-azure.txt', 1),
+(12, 'mounted and created', 1),
+(13, 'android.jpg', 1),
+(14, 'android.jpg', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `last_name`, `email`, `password`) VALUES
+(1, 'moha', 'mohamed@gmail.com', '123456789'),
+(2, 'testuser', 'test@gmail.com', '1234567');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `file`
+--
+ALTER TABLE `file`
+  ADD CONSTRAINT `file_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
