ca61ec8642898a061db159a331df7e944376c0de
Souissi Mohamed <msouissi@amfinesoft.com>
2020-01-25 22:40:20 +0100

    app-service terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "projet_cloud" {
+resource "azurerm_resource_group" "project_cloud" {
   name     = "project_cloud"
   location = "francecentral"
 
@@ -7,11 +7,32 @@ resource "azurerm_resource_group" "projet_cloud" {
   }
 }
 
-resource "azurerm_storage_account" "projet_cloud" {
+resource "azurerm_storage_account" "project_cloud" {
   name                     = "storagecloudx"
-  resource_group_name      = "${azurerm_resource_group.projet_cloud.name}"
+  resource_group_name      = "${azurerm_resource_group.project_cloud.name}"
   location                 = "francecentral"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+
+resource "azurerm_app_service_plan" "project_cloud" {
+    name                = "cloudXAppServicePlan"
+    location            = "${azurerm_resource_group.project_cloud.location}"
+    resource_group_name = "${azurerm_resource_group.project_cloud.name}"
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "project_cloud" {
+    name                = "cloudXAppService"
+    location            = "${azurerm_resource_group.project_cloud.location}"
+    resource_group_name = "${azurerm_resource_group.project_cloud.name}"
+    app_service_plan_id = "${azurerm_app_service_plan.project_cloud.id}"
+    app_settings = {
+    "WEBSITE_NODE_DEFAULT_VERSION" = "10 LTS"
+  }
 }
 
