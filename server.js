94947f77348d6d71a8b808255b95398d9bbc51cb
Souissi Mohamed <mohaxoy98@gmail.com>
2020-02-05 23:30:17 +0100

    test

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.get('/find-all-files', verifyToken, (req,res) => {
 
 app.get('/all-users', (request, response) => {
   let flashDataBase = require("./models/flashDataBase")
-  flashDataBase.all({}, function (err, user) {
+  flashDataBase.allUsers({}, function (err, user) {
       response.json(result);
   });
 });
